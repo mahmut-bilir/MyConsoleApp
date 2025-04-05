a280c99c1ad3fdbae61d9c112b1dd503ec01c503
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-04-05 22:14:18 +0300

    Usage of Newtonsoft.Json

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Hi, .Net Core!");
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hi, .Net Core!");
 
 Console.Write("Enter your name: ");
 string? name = Console.ReadLine();
 
 Console.WriteLine("Hi, " + name);
+
+
+var person = new { Name = "Mahmut", Age = 30 };
+string strPerson = JsonConvert.SerializeObject(person);
+Console.WriteLine(strPerson);
